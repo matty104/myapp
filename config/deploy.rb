cc4b1e29cf3fdeb5fab33f02b1252e7f94fce17c
Matt Elhotiby <Matt@Matts-Computer.local>
2009-10-18 23:00:43 -0400

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,65 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "myapp"
+  set :user, "root"
+  set :use_sudo, false
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+  set :repository,  "git@github.com:matty104/myapp.git"
+  
+  set :deploy_to, "/srv/#{application}"
+  set :scm, :git
+  set :git_enable_submodules, 1         # Make sure git submodules are populated
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+  set :location, "ec2-174-129-145-33.compute-1.amazonaws.com"
+  role :app, location
+  role :web, location
+  role :db,  location, :primary => true
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+  namespace :deploy do
+    desc "Restart Application"
+    task :restart, :roles => :app do
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+
+    desc "Make symlink for database.yml" 
+    task :symlink_dbyaml do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+    end
+
+    desc "Create empty database.yml in shared path" 
+    task :create_dbyaml do
+      run "mkdir -p #{shared_path}/config" 
+      put '', "#{shared_path}/config/database.yml" 
+    end
+
+    desc "Make symlink for s3.yml" 
+    task :symlink_s3yaml do
+      run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml" 
+    end
+
+    desc "Create empty s3.yml in shared path" 
+    task :create_s3yaml do
+      run "mkdir -p #{shared_path}/config" 
+      put '', "#{shared_path}/config/s3.yml" 
+    end
+
+    desc "Make symlink for billing_on_rails.yml" 
+    task :symlink_billingyaml do
+      run "ln -nfs #{shared_path}/config/billing_on_rails.yml #{release_path}/config/billing_on_rails.yml" 
+    end
+
+    desc "Create empty billing_on_rails.yml in shared path" 
+    task :create_billingyaml do
+      run "mkdir -p #{shared_path}/config" 
+      put '', "#{shared_path}/config/billing_on_rails.yml" 
+    end
+  end
+
+  after 'deploy:setup', 'deploy:create_dbyaml'
+  after 'deploy:update_code', 'deploy:symlink_dbyaml'
+
+  after 'deploy:setup', 'deploy:create_s3yaml'
+  after 'deploy:update_code', 'deploy:symlink_s3yaml'
+
+  after 'deploy:setup', 'deploy:create_billingyaml'
+  after 'deploy:update_code', 'deploy:symlink_billingyaml'
+
+  after "deploy", "deploy:cleanup"
